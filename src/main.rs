baf94b67a3318811963f433006349bcdd9d4e9c6
Tyler Dickson <tylercdickson@gmail.com>
2026-02-23 21:59:58 -0700

    working embedded 'Hello World'

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,22 @@
 use cortex_m_rt::entry;
 use panic_halt as _;
 
+use embedded_hal::{delay::DelayNs, digital::OutputPin};
+use microbit::{Board, hal::Timer };
+
 #[entry]
 fn main() -> ! {
-    loop {}
+    let mut board = Board::take().unwrap();
+
+    let _ = board.display_pins.col1.set_low();
+    let mut led = board.display_pins.row1;
+
+    let mut timer = Timer::new(board.TIMER0);
+
+    loop {
+        let _ = led.set_low();
+        timer.delay_ms(1_000);
+        let _ = led.set_high();
+        timer.delay_ms(1_000);
+    }
 }
